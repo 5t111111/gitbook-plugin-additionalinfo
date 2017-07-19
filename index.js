a81611095eabf51f8b03915f3c5caf037073bd26
Hirofumi Wakasugi <baenej@gmail.com>
2017-07-19 14:28:13 +0900

    Update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 module.exports = {
   hooks: {
     "page:before": function(page) {
-      page.content = "```\n" +
+      page.content = "```" +
+        "\n" +
         "Crystal の日本語ドキュメントはしばらく更新されておらず情報が古くなっているため、\n" +
         "できるだけ[原文の英語ドキュメント](https://crystal-lang.org/docs/)で最新の情報を参照ください。\n" +
         "\n" +
-        "```" +
+        "```\n" +
+        "\n" +
         page.content;
       return page;
     }
